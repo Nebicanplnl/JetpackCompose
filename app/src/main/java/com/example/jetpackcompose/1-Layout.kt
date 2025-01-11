28371a76cbcf9450ba24c1efc774eaec69f97252
Nebican Planalı <nebicanplanali@gmail.com>
2025-01-11 23:32:25 +0300

    LayoutFile added

---
+++ app/src/main/java/com/example/jetpackcompose/1-Layout.kt
@@ -0,0 +1,15 @@
+package com.example.jetpackcompose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ColumnSamples() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ColumnSamplesPreview() {
+    ColumnSamples()
+}
