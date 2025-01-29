98c86cfa9c98f26f855b3dd6019cf80b883da766
Nebican Planalı <nebicanplanali@gmail.com>
2025-01-29 23:25:40 +0300

    Row

--- app/src/main/java/com/example/jetpackcompose/1-Layout.kt
+++ app/src/main/java/com/example/jetpackcompose/1-Layout.kt
@@ -1,11 +1,38 @@
 package com.example.jetpackcompose
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ColumnSamples() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(text ="İtem one")
+        Text(text ="İtem two")
+        Text(text ="İtem three")
+    }
+}
 
+@Composable
+fun RowSamples() {
+    Row(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text ="İtem one")
+        Text(text ="İtem two")
+        Text(text ="İtem three")
+    }
 }
 
 @Preview(showBackground = true)
@@ -13,3 +40,10 @@ fun ColumnSamples() {
 fun ColumnSamplesPreview() {
     ColumnSamples()
 }
+
+@Preview(showBackground = true)
+@Composable
+fun RowSamplesPreview() {
+    RowSamples()
+}
+
